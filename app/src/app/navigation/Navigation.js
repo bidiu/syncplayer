d16e62d5d5c086738b79341225ec3d21b40fee98
bedew <sunhe1007@126.com>
2018-12-12 21:33:45 -0500

    styled the nav bar

--- app/src/app/navigation/Navigation.js
+++ app/src/app/navigation/Navigation.js
@@ -1,41 +1,53 @@
 import  React, { Component } from 'react';
+import { connect } from "react-redux";
+import { withRouter } from 'react-router';
 import { NavLink } from "react-router-dom";
 
 import './Navigation.css';
 import longLogo from '../assets/long_logo.svg';
+import shortLogo from '../assets/short_logo.svg';
 
-// <Link to="/">Home</Link>
 class Navigation extends Component {
   render() {
+    let logoUrl = this.props.viewportType === 'VIEWPORT_MOBILE' ?
+      shortLogo : longLogo;
+
     return (
       <div className="Navigation">
-        {/* logo */}
-        <div className="Navigation-logo">
-          <a href="/"><img src={longLogo} alt="logo" /></a>
-        </div>
+        <div className="Navigation-flex-container">
+          {/* logo */}
+          <div className="Navigation-logo">
+            <a href="/"><img src={logoUrl} alt="logo" /></a>
+          </div>
 
-        {/* room history */}
-        <div className="Navigation-tab">
-          <NavLink 
-            className="Navigation-text-link" 
-            activeClassName="Navigation-text-link-selected" 
-            to="/rooms">
-            Rooms
-          </NavLink>
-        </div>
+          {/* room history */}
+          <div className="Navigation-tab">
+            <NavLink 
+              className="Navigation-text-link" 
+              activeClassName="Navigation-text-link-selected" 
+              to="/rooms">
+              Rooms
+            </NavLink>
+          </div>
 
-        {/* about */}
-        <div className="Navigation-tab">
-          <NavLink
-            className="Navigation-text-link" 
-            activeClassName="Navigation-text-link-selected" 
-            to="/about">
-            About
-          </NavLink>
+          {/* about */}
+          <div className="Navigation-tab">
+            <NavLink
+              className="Navigation-text-link" 
+              activeClassName="Navigation-text-link-selected" 
+              to="/about">
+              About
+            </NavLink>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default Navigation;
+export default withRouter(connect(
+  state => ({
+    viewportType: state.ui.viewportType
+  }),
+  dispatch => ({})
+)(Navigation));
