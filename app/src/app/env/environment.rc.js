7c57c2738d3e7d32c134abb51451ec62e41a077d
bedew <sunhe1007@126.com>
2019-01-01 23:03:46 -0500

    fixed a bug related to env variable

--- app/src/app/env/environment.rc.js
+++ app/src/app/env/environment.rc.js
@@ -4,7 +4,7 @@ export default {
   //   'ws://45.79.182.46:1113'
   // ],
   apiBase: '/api/v1',
-  syncServerUrl: process.env.SYNC_SERVER_URL || 'ws://127.0.0.1:80/ws',
+  syncServerUrl: process.env.REACT_APP_SYNC_SERVER_URL || 'ws://127.0.0.1:80/ws',
   demoVideoInfo: {
     videoUrl: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
     videoType: 'mp4',
