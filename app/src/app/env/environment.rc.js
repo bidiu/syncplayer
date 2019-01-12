8945d123b0d6a021264158840bfadf2f93fc4441
bedew <sunhe1007@126.com>
2019-01-12 12:00:53 -0500

    chore

--- app/src/app/env/environment.rc.js
+++ app/src/app/env/environment.rc.js
@@ -1,13 +1,10 @@
 export default {
   name: 'release candidate',
-  // initPeerUrls: [
-  //   'ws://45.79.182.46:1113'
-  // ],
   apiBase: '/api/v1',
   syncServerUrl: process.env.REACT_APP_SYNC_SERVER_URL || 'ws://127.0.0.1:80/ws',
   demoVideoInfo: {
-    videoUrl: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
-    videoType: 'mp4',
+    videoUrl: 'EDPV-Syvx_k',
+    videoType: 'youtube',
     pageUrl: undefined
   }
 };
