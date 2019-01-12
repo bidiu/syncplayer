c8a603ccf0d3e3695d59c810273f6fc0f7a3494d
bedew <sunhe1007@126.com>
2019-01-11 23:40:24 -0500

    chore: styles

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -16,7 +16,6 @@ import Footer from './footer/Footer';
 import Room from './room/Room';
 import { getScrollBarWidth, getViewportDimension } from './utils/domUtils';
 import { getPayload } from './utils/epicUtils';
-import { getTransitionColor } from './utils/colorUtils';
 import resources from './common/rest/resources';
 import env from './env/environment';
 
@@ -24,8 +23,6 @@ import { addDemoRoom, syncRooms } from './common/state/rooms/index';
 import { setScrollbarWidth, setViewportSize } from './common/state/newui/index';
 
 import './App.css';
-
-const bodyBgDarkColor = '#FF5445';
 
 const getInitRooms = () => {
   return JSON.stringify({
@@ -66,29 +63,29 @@ class App extends Component {
           this.props.setViewportSize(width, height);
         }),
       
-      Observable.fromEvent(window, 'scroll', { passive: true })
-        .subscribe(() => {
-          requestAnimationFrame(() => {
+      // Observable.fromEvent(window, 'scroll', { passive: true })
+      //   .subscribe(() => {
+      //     requestAnimationFrame(() => {
   
-            if (this.props.location.pathname !== '/') { return; }
+      //       if (this.props.location.pathname !== '/') { return; }
   
-            let vh = this.props.viewportSize.height;
-            if (!vh) { return; }
-            let low = Math.round(vh * 0.5);
-            let high = Math.round(vh * 0.8);
-            let cur = window.scrollY;
+      //       let vh = this.props.viewportSize.height;
+      //       if (!vh) { return; }
+      //       let low = Math.round(vh * 0.5);
+      //       let high = Math.round(vh * 0.8);
+      //       let cur = window.scrollY;
   
-            if (cur < low) {
-              document.body.style.backgroundColor = '';
-            } else if (cur > high) {
-              document.body.style.backgroundColor = bodyBgDarkColor;
-            } else {
-              let percentage = (cur - low) / (high - low);
-              let transitionColor = getTransitionColor('#ffffff', bodyBgDarkColor, percentage);
-              document.body.style.backgroundColor = transitionColor;
-            }
-          }); // end of requestAnimationFrame
-        }) // end of subscribe
+      //       if (cur < low) {
+      //         document.body.style.backgroundColor = '';
+      //       } else if (cur > high) {
+      //         document.body.style.backgroundColor = bodyBgDarkColor;
+      //       } else {
+      //         let percentage = (cur - low) / (high - low);
+      //         let transitionColor = getTransitionColor('#ffffff', bodyBgDarkColor, percentage);
+      //         document.body.style.backgroundColor = transitionColor;
+      //       }
+      //     }); // end of requestAnimationFrame
+      //   }) // end of subscribe
     ];
   }
 
--- app/src/app/env/environment.dev.js
+++ app/src/app/env/environment.dev.js
@@ -1,11 +1,7 @@
 export default {
   name: 'devlopment',
-  initPeerUrls: [
-    'ws://45.79.182.46:1113'
-  ],
-  apiBase: 'http://192.168.0.14:4010/api/v1',
-  // syncServerUrl: 'ws://localhost:4010'
-  syncServerUrl: 'ws://192.168.0.14:4010',
+  apiBase: 'http://localhost:4010/api/v1',
+  syncServerUrl: 'ws://127.0.0.1:4010',
   demoVideoInfo: {
     videoUrl: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
     videoType: 'mp4',
